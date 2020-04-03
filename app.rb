4bc38f933fb4fc4fd6a812c5d9bd1765956db7e9
Dan Foley <dan@dan-foley.com>
2020-04-03 19:20:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,7 +32,7 @@ class App < Sinatra::Base
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
 
-  get '/:operation/:num/:num' do
+  get '/:operation/:num1/:num2' do
     @operation = params[:operation]
     @num1 = params[:num1].to_i
     @num2 = params[:num2].to_i
