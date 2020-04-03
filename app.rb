8c6d1969b18bd6e585b441f5e3d73ffcc838f448
Dan Foley <dan@dan-foley.com>
2020-04-03 18:50:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,8 +14,8 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    # @num = params[:number].to_i
-    # @phrase = params[:phrase]
+    @num = params[:number].to_i
+    @phrase = params[:phrase]
     # @num.times do
     #   "#{@phrase}"
     # end
